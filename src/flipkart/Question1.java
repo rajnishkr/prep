5f9be0325361873f3e93849f4661b6d7f02cebb2
Rajnish Kumar <rajnish.kr123@gmail.com>
2018-01-02 22:24:04 -0600

    Beautiful Sub Array

---
+++ src/flipkart/Question1.java
@@ -0,0 +1,64 @@
+package flipkart;
+
+import java.util.Scanner;
+
+/**
+ * Beautiful odds
+ **/
+public class Question1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int input[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            input[i] = sc.nextInt();
+        }
+        int m = sc.nextInt();
+
+
+        int solution = getSolution(n, input, m);
+        System.out.println(solution);
+
+    }
+
+    private static int getSolution(int n, int[] input, int m) {
+        int dp[] = new int[n + 1];
+        int solution = 0;
+        int start = 0;
+        int prevOne = -1;
+        int countOfOdds = 0;
+        dp[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            int temp=solution;
+            if (input[i] % 2 != 0) {
+                if (prevOne == -1) {
+                    prevOne = i;
+                }
+                countOfOdds++;
+                if (countOfOdds == m) {
+                    solution += (prevOne - start) + 1;
+
+                } else if (countOfOdds > m) {
+                    start = prevOne;
+                    prevOne++;
+                    countOfOdds--;
+                    while (input[prevOne] % 2 == 0 && prevOne < i) {
+                        prevOne++;
+                    }
+                    solution += prevOne - start;
+
+                }
+            } else {
+                if (countOfOdds == m) {
+                    solution += dp[i - 1];
+                }
+            }
+
+            dp[i] = solution-temp;
+
+        }
+        return solution;
+    }
+}
